66139f622401a5cf35a45841b6f159be8a3e4245
Tanvi Dua <tanvidua5774@gmail.com>
2025-06-25 13:40:37 +0530

    Baseline course designed

--- course/src/courses/models.py
+++ course/src/courses/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class AccessRequirement(models.TextChoices):
+    ANYONE = "any", "Anyone"
+    EMAIL_REQUIRED = "email", "Email required"
+
+class PublishStatus(models.TextChoices):
+    PUBLISHED = "publish", "Published"
+    COMING_SOON = "soon", "Coming Soon"
+    DRAFT = "draft", "Draft"
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    # uuid = models.UUIDField(default=uuid.uuid1, unique=True)
+    public_id = models.CharField(max_length=130, blank=True, null=True, db_index=True)
+    access = models.CharField(
+        max_length=10, 
+        choices=AccessRequirement.choices,
+        default=AccessRequirement.EMAIL_REQUIRED
+    )
+    status = models.CharField(
+        max_length=10, 
+        choices=PublishStatus.choices,
+        default=PublishStatus.DRAFT
+    )
+
+    # @property decorator in Python is used to define a method that you can access like an attribute — without needing parentheses ()
+    # Using @property makes it read-only
+    @property    
+    def is_published(self):
+        return self.status == PublishStatus.PUBLISHED
